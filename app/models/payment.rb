d8c85270011e69a1fbc8e3338112835ad4b49674
Nelli1985 <nelli.vassila@khk.ee>
2018-10-18 09:05:14 +0300

    messaging option and API call

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,2 +1,26 @@
-class Payment < ApplicationRecord
+class Payment
+    def request
+        http.request(req)
+    end
+    def req
+        req2 = Net::HTTP::Post.new(uri.path, 'Content-Type' => 'application/json')
+        req2.body = body.to_json
+        req2
+    end
+    def uri
+        URI('http://vso17.cariba.ee/payments.json')
+    end
+    def http
+        Net::HTTP.new(uri.host, uri.port)
+    end
+    def body 
+        { 
+            "sender_account": "nelli.vassila@khk.ee",
+            "recipient_name": "Nelli.Vassila",
+            "recipient_account": Event.first.name,
+            "description": "description",
+            "amount": 1,
+            "token": "2899cfab862af6858bb8c2eb174da0fe"
+        }
+    end
 end
