6cd08500cd6c9c832bd4ad97e68ea8f47b65ecdd
Nelli1985 <nelli.vassila@khk.ee>
2018-10-18 14:46:22 +0300

    Time api

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,15 @@
+class ApiController < ApplicationController
+  skip_before_action :authenticate_user!
+  def about
+  	require 'date'
+
+    current_time = DateTime.now
+
+  	render json: {
+  		time: current_time.strftime("%H:%M"),
+  		date: current_time.strftime("%Y-%m-%d"),
+  		name: 'Kala Ahven',
+  		fruits: ['banana', 'watermelon']
+  	}
+  end
+end
